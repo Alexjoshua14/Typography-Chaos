9c67c051345da0395247d6773267af792bb1d6d4
Alex <ajoshua14@icloud.com>
2023-12-18 12:31:21 -0800

    Complete initial version of animating chaos points

--- nextjs-app/src/components/ChaosWrapper.tsx
+++ nextjs-app/src/components/ChaosWrapper.tsx
@@ -4,6 +4,11 @@ import { FC } from 'react'
 
 import ChaosString from './ChaosString'
 import { ChaosDictionary } from '@/lib/ChaosDictionary'
+import { Point } from '@/lib/validators/Point'
+import { chaosPosition, randomPosition } from '@/lib/ChaosPointAnimation'
+
+const FRAME_RATE = 24
+const FRAME_COUNT = FRAME_RATE * 8
 
 interface ChaosWrapperProps {
   text: string
@@ -14,8 +19,57 @@ const ChaosWrapper: FC<ChaosWrapperProps> = async ({ text }) => {
   await chaosDictionary.fetchLetters(text)
   console.log(chaosDictionary)
 
+  let width = 0
+  let height = 0
+  text.split('').forEach((letter, index) => {
+    width += chaosDictionary.get(letter)?.bounding_box.width ?? 0
+    height = Math.max(height, chaosDictionary.get(letter)?.bounding_box.height ?? 0)
+  })
+
+  const animationFrames: Point[][] = []
+
+  // Generate animation frames
+
+  const finalAnimationFrame: Point[] = []
+
+  let leftOffset = 0
+
+  text.split('').forEach((letter, index) => {
+    const l = chaosDictionary.get(letter) ?? { bounding_box: { width: 0, height: 0 }, points: [] }
+    l.points.forEach((point) => {
+      const x = point[0] + leftOffset
+      finalAnimationFrame.push([x, point[1], point[2]])
+    })
+    leftOffset += l.bounding_box.width
+  })
+
+  const initialAnimationFrame: Point[] = []
+  for (let i = 0; i < finalAnimationFrame.length; i++) {
+    initialAnimationFrame.push(randomPosition(finalAnimationFrame[i], { width, height }))
+  }
+
+  text.split('').forEach((letter, index) => {
+    const points = chaosDictionary.get(letter)?.points ?? []
+    points.forEach((point) => {
+      initialAnimationFrame.push(point)
+    })
+  })
+
+  for (let i = 1; i < FRAME_COUNT; i++) {
+    const animationFrame: Point[] = []
+    for (let j = 0; j < finalAnimationFrame.length; j++) {
+      animationFrame.push(chaosPosition(initialAnimationFrame[j], finalAnimationFrame[j], i, FRAME_COUNT))
+    }
+    animationFrames.push(animationFrame)
+  }
+
+  animationFrames.push(finalAnimationFrame)
+
+  console.log(`Animation frame length: ${animationFrames.length}`)
+
+
   return (
-    <ChaosString text={text} chaosDictionary={chaosDictionary.dict} />
+    <ChaosString text={text} animationFrames={animationFrames} width={width} height={height} frameCount={FRAME_COUNT} frameRate={FRAME_RATE} />
   )
 }
 
