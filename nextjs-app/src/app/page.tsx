869f64deac01ad111ee59fd85abc16e5e924960f
Alex <ajoshua14@icloud.com>
2023-12-18 15:00:12 -0800

    Wrap components in useMemo and implement suspense boundary

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,19 +1,18 @@
-import Image from 'next/image'
-import ChaosLetter from '@/components/ChaosLetter'
-import { getChaosCoordinates } from '@/lib/ChaosCoordinates'
-import ChaosString from '@/components/ChaosString'
+import { Suspense, useMemo } from 'react'
+
 import ChaosWrapper from '@/components/ChaosWrapper'
 
 export default async function Home() {
-  const points = await getChaosCoordinates('A')
 
   return (
     <main className="flex min-h-screen h-[400vh] flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <div className="fixed">
-          <ChaosWrapper text="Hello" />
-          <ChaosWrapper text="World!" />
-        </div>
+        <Suspense fallback={<div>Loading..</div>}>
+          <div className="fixed">
+            {useMemo(() => <ChaosWrapper text="Hello" />, [])}
+            {useMemo(() => <ChaosWrapper text="World!" />, [])}
+          </div>
+        </Suspense>
       </div>
     </main>
   )
