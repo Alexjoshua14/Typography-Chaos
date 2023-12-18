c667cacc39ea321dfff829373f5909b7ef88508b
Alex <ajoshua14@icloud.com>
2023-12-17 16:22:15 -0800

    Lazily add test for process_input_string()

--- python/src/tests/test_script.py
+++ python/src/tests/test_script.py
@@ -2,11 +2,15 @@
 # Import the text_to_point_coordinates function
 from chaos.render_text import text_to_point_coordinates
 from chaos.render_text import get_font_file
+from main import process_input_string
 
 import json
 
 result = text_to_point_coordinates("A")
 print(result.to_dict())
+
+processed_str = process_input_string("B")
+print(processed_str)
 
 # JSONify the result and print it in pretty format
 # Convert result.to_dict() to a JSON string
