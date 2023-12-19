a55e7a79afa49ddf67bf19b78bd157c6ef5f70d2
Alex <ajoshua14@icloud.com>
2023-12-18 18:56:00 -0800

    Standardize python file names

--- python/src/tests/test_fontFile.py
+++ python/src/tests/test_fontFile.py
@@ -1,5 +1,14 @@
 # Test available_fonts() function
-from utils.font_file import available_fonts
+from utils.font_file import available_fonts, get_font_file
+import os
 
-res = available_fonts()
-print(res)
+font_options = available_fonts()
+print(font_options)
+
+for font in font_options:
+  # Get font file and check if it exists
+  font_file = get_font_file(os.path.dirname(os.path.realpath(__file__)), font, "Regular")
+  if (not os.path.isfile(font_file)):
+    raise Exception("Font file does not exist")
+  
+print("All font files exist")
