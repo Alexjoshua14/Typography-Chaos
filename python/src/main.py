3cbdc116daa7eb2d2709575bde648852ae2bbe56
Alex <ajoshua14@icloud.com>
2023-12-20 17:44:18 -0800

    Begin implementing a means to fetch multiple individual characters in one request

--- python/src/main.py
+++ python/src/main.py
@@ -4,6 +4,7 @@ from typing import List, Tuple, Optional, Dict, Union
 
 from chaos.render_text import text_to_point_coordinates
 from models.chaos_character import ChaosCharacter
+from models.chaos_dictionary import ChaosDictionary
 from utils.font_file import available_fonts
 
 app = FastAPI()
@@ -16,7 +17,7 @@ class InputData(BaseModel):
 class OutputData(BaseModel):
   result: Dict[str, Union[Dict[str, int], List[Tuple[int, int, Optional[int]]]]]
 
-@app.put('/chaos-letter', response_model=OutputData)
+@app.put('/chaosLetter', response_model=OutputData)
 async def chaos_letter(data: InputData):
   try:
     input_string = data.input_string
@@ -24,6 +25,28 @@ async def chaos_letter(data: InputData):
     size = data.size
     
     return process_input_string(input_string, font, size)
+    
+  except Exception as e:
+    if (str(e) == "Font file does not exist"):
+      raise HTTPException(status_code=400, detail=str(e))
+    
+    raise HTTPException(status_code=500, detail=str(e))
+
+@app.put('/chaosString', response_model=OutputData)
+async def chaos_string(data: InputData):
+  try:
+    input_string = data.input_string
+    font = data.font
+    size = data.size
+    
+    chaos_dictionary = ChaosDictionary()
+    
+    for letter in input_string:
+      chaos_character = text_to_point_coordinates(letter, font, size)
+      chaos_dictionary.put(letter, chaos_character)
+    
+    print(chaos_dictionary.to_dict())
+    return {'result': chaos_dictionary}
     
   except Exception as e:
     if (str(e) == "Font file does not exist"):
