b67ae241870be33d8ac8714a058b7077a3be89a6
Alex <ajoshua14@icloud.com>
2023-12-20 17:43:12 -0800

    Clean up imports

--- nextjs-app/src/components/ChaosString.tsx
+++ nextjs-app/src/components/ChaosString.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import { getChaosCoordinates } from '@/lib/ChaosCoordinates'
-import { FC, MutableRefObject, useEffect, useRef, useState } from 'react'
-import ChaosLetter from './ChaosLetter'
+import { FC, useRef } from 'react'
 import { Point } from '@/lib/validators/Point'
-import { ChaosCharacter } from '@/lib/validators/ChaosCharacter'
-import { ChaosDictionary } from '@/lib/ChaosDictionary'
 import { useAnimation } from '@/hooks/useAnimation'
 import { AnimationType } from '@/lib/validators/AnimationType'
-
 
 interface ChaosStringProps {
   text: string
